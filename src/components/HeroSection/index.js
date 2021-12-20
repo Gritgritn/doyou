850e4f8ccc7b4d7e4a64730399181448c3c1c7e0
Grit <perepadin00@gmail.com>
2021-12-20 19:58:10 +0300

    add HeroSection

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroBtnWrapper, HeroH1, HeroP, ArrowForward, ArrowRight } from './HeroElements'
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElement'
+
+const HeroSection = () => {
+    const [hover, sethover] = useState(false);
+
+    const onHover = () => {
+        sethover(!hover);
+    }
+
+    return (
+        <HeroContainer id="home">
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Виртуальный банкинг становится доступным</HeroH1>
+                <HeroP>
+                    Зарегистрируйте новый аккаунт сегодня и получите 250$ на кредитном счету для ваших покупок.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
+                        Начать {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
