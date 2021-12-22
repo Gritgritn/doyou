b25db632469d254eb9a981646c31943605a7f425
Grit <perepadin00@gmail.com>
2021-12-22 18:58:11 +0300

    Create InfoSection

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,67 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDescr: true,
+    topLine: 'Премиальный Банк',
+    headline: 'Безлимитное количество транзакций без комисии',
+    description: 'Получите доступ к нашему сервису, чтобы получить доступ к транзакций не облагаемыми коммисиями.',
+    buttonLabel: 'Начать',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Man',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDescr: false,
+    topLine: 'Безлимитный доступ',
+    headline: 'Безлимитное количество транзакций без комисии',
+    description: 'Получите доступ к нашему сервису, чтобы получить доступ к транзакций не облагаемыми коммисиями.',
+    buttonLabel: 'Узнать больше',
+    imgStart: true,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDescr: false,
+    topLine: 'Премиальный Банк',
+    headline: 'Безлимитное количество транзакций без комисии',
+    description: 'Получите доступ к нашему сервису, чтобы получить доступ к транзакций не облагаемыми коммисиями.',
+    buttonLabel: 'Присоединиться',
+    imgStart: false,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Card',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjFour = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDescr: false,
+    topLine: 'Безлимитный доступ',
+    headline: 'Безлимитное количество транзакций без комисии',
+    description: 'Получите доступ к нашему сервису, чтобы получить доступ к транзакций не облагаемыми коммисиями.',
+    buttonLabel: 'Узнать больше',
+    imgStart: true,
+    img: require('../../images/svg-4.svg'),
+    alt: 'UserExperience',
+    dark: false,
+    primary: false,
+    darkText: true
+}
